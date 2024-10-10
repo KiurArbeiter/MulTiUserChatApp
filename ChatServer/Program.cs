17925823010b6b46f02ec94e132dd0a0a6ef0757
Opilane TTHK <opilane@tthk.ee>
2024-10-10 10:47:54 +0300

    ChatApp
    First commit -22min

---
+++ ChatServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chatserver
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            static void Main(String[] args)
+            {
+                _users = new List<Client>();
+                _listener = new TcpListener(IPAddress.Parse("127.0.0.1", 7891));
+                _listener.Start();
+
+                while (true)
+                {
+                    var client = new Client(_listener.AcceptTcpClient());
+                    _users.Add(client);
+                }
+
+            }
+        }
+    }
+}
